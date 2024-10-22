8d80a7c7f45ba0493e246551d199606411975d03
ahmed_khaled <ahmedkhldosman99@gmail.com>
2024-10-22 17:39:54 +0300

    The Project Iphone

---
+++ index.js
@@ -0,0 +1,9 @@
+let num1 = document.querySelector(".img");
+let body = document.querySelector("body")
+function phones(phone){
+    num1.src = phone;
+}
+
+function colors(color){
+    body.style.backgroundColor = color;
+}
